8537ef9a1e2da7a3cbfcc66a955740fb64ef9093
sagaauroragz <saga.grantz@gmail.com>
2025-09-30 23:54:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,20 @@ const card = document.getElementById("card");
 const text = document.getElementById("text");
 
 const compliments = [
-  "you’re awesome",
+  "you're awesome",
   "you make things brighter",
-  "you’re doing great",
+  "you're doing great",
   "you're a badass bitch",
   "you are enough"
 ];
 
+let index = 0; // start at the first compliment
+
 card.addEventListener("click", () => {
-  const random = compliments[Math.floor(Math.random() * compliments.length)];
-  text.textContent = random;
+  text.textContent = compliments[index]; // show current compliment
+  index++; // move to the next one
+
+  if (index >= compliments.length) {
+    index = 0; // reset to start when we reach the end
+  }
 });
